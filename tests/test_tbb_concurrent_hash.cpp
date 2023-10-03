708cef8293c9bf55ee4b4f543c484ec83aa443eb
Zhiting Zhu <zhitingz@cs.utexas.edu>
2023-10-03 19:07:23 +0000

    checking cxl allocator can work
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- tests/test_tbb_concurrent_hash.cpp
+++ tests/test_tbb_concurrent_hash.cpp
@@ -1,14 +1,17 @@
 #include <boost/interprocess/shared_memory_object.hpp>
 #include <boost/interprocess/managed_shared_memory.hpp>
 #include "oneapi/tbb/concurrent_hash_map.h"
+#include "cxlalloc.hpp"
 // #include <libcuckoo/cuckoohash_map.hh>
 
 namespace bip = boost::interprocess;
 
 int main(void) {
+	typedef std::basic_string<char, std::char_traits<char>, CXLAlloc<char>> cxl_string;
 	typedef bip::allocator<std::string, bip::managed_shared_memory::segment_manager> ShmAlloc;
 	// typedef bip::allocator<std::pair<std::string, std::string>, bip::managed_shared_memory::segment_manager> CuckooShmAlloc;
 	typedef oneapi::tbb::concurrent_hash_map<std::string, std::string, ShmAlloc> HashMap;
+	typedef oneapi::tbb::concurrent_hash_map<cxl_string, cxl_string, CXLAlloc<cxl_string>> CxlAllocHashMap;
 	// typedef libcuckoo::cuckoohash_map<std::string, std::string, std::hash<std::string>, std::equal_to<std::string>, CuckooShmAlloc> CuckooMap;
 
 	// declare memory
@@ -22,4 +25,6 @@ int main(void) {
 	// construct the queue in shared memory
 	HashMap *map = segment.construct<HashMap>("data_queue")(alloc_inst);
 	// CuckooMap *map_cuckoo = segment2.construct<CuckooMap>("cuckoo_map")(libcuckoo::DEFAULT_SIZE, std::hash<std::string>(), std::equal_to<std::string>(), cuckoo_inst);
+	cxlalloc_init("region", 1024*1024*1024, 2, 1);
+	CxlAllocHashMap cxl_hash_map;
 }
